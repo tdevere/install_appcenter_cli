4bea9dc7a3488259bcb6c4b9d6f1260a40d3f017
tdevere <tdevere@microsoft.com>
2021-08-19 18:00:19 -0500

    echo "$APPCENTER_VERSION"

--- 53577900040616/53577900040616.Android/appcenter-post-clone.sh
+++ 53577900040616/53577900040616.Android/appcenter-post-clone.sh
@@ -1,10 +1,12 @@
 ﻿echo "****************************************"
+echo "$APPCENTER_VERSION"
 echo "AFTER CLONE"
 APPCENTER_VERSION=2.9.0
 echo "Before installing latest appcenter"
 appcenter -v
 echo "Installing app center cli"
 #npm install -g "appcenter-cli@2.9.0"
+echo "$APPCENTER_VERSION"
 npm install -g "appcenter-cli@$APPCENTER_VERSION"
 echo "After installing latest appcenter"
 appcenter -v
