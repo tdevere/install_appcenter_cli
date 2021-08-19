0c6d5aec8dd5cb81282fadd0401d9297df65c1d3
tdevere <tdevere@microsoft.com>
2021-08-19 18:05:13 -0500

    customer code

--- 53577900040616/53577900040616.Android/appcenter-post-clone.sh
+++ 53577900040616/53577900040616.Android/appcenter-post-clone.sh
@@ -1,12 +1,20 @@
 ﻿echo "****************************************"
-echo "$APPCENTER_VERSION"
 echo "AFTER CLONE"
+NODE_VERSION=node13
 APPCENTER_VERSION=2.9.0
+
+echo "Installing NVM..."
+brew install nvm
+source $(brew --prefix nvm)/nvm.sh
+nvm list
+nvm use --delete-prefix ${NODE_VERSION}
+nvm alias default ${NODE_VERSION}
+echo "Node version..."
+node -v
+echo "All set with node version"
 echo "Before installing latest appcenter"
 appcenter -v
 echo "Installing app center cli"
-#npm install -g "appcenter-cli@2.9.0"
-echo "$APPCENTER_VERSION"
 npm install -g "appcenter-cli@$APPCENTER_VERSION"
 echo "After installing latest appcenter"
 appcenter -v
