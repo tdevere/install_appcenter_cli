530e91f2be613a87038ab450bc8ac75dbac776b2
tdevere <tdevere@microsoft.com>
2021-08-19 17:40:13 -0500

    added script

---
+++ 53577900040616/53577900040616.Android/appcenter-post-clone.sh
@@ -0,0 +1,12 @@
+﻿echo "****************************************"
+echo "AFTER CLONE"
+
+APPCENTER_VERSION=2.9.0
+
+echo "Before installing latest appcenter"
+appcenter -v
+echo "Installing app center cli"
+npm install -g "appcenter-cli@latest"
+echo "After installing latest appcenter"
+appcenter -v
+
